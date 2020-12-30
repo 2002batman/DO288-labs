eb69123aca20342d8a0c6f6734bfdcebc66dde94
Enoch Lee <enoch.ymlee@gmail.com>
2020-12-30 17:24:37 +0800

    2030

--- labs/build-template/create-app.sh
+++ labs/build-template/create-app.sh
@@ -2,7 +2,7 @@
 
 source /usr/local/etc/ocp4.config
 
-oc new-app  --as-deployment-config --template ${RHT_OCP4_DEV_USER}-common/php-mysql-ephemeral \
+oc new-app  --as-deployment-config --template php-mysql-ephemeral \
   -p NAME=quotesapi \
   -p APPLICATION_DOMAIN=quote-${RHT_OCP4_DEV_USER}.${RHT_OCP4_WILDCARD_DOMAIN} \
   -p SOURCE_REPOSITORY_URL=https://github.com/${RHT_OCP4_GITHUB_USER}/DO288-apps \
